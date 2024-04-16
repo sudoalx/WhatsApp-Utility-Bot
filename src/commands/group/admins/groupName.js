a111d806adc495f07ed78fcfb914a73a7797e564
Alex <alex@alex-dev.me>
2024-04-16 11:30:45 -0600

    Refactor group name change command to improve error handling and add reaction messages

--- src/commands/group/admins/groupName.js
+++ src/commands/group/admins/groupName.js
@@ -1,10 +1,16 @@
+const { reactToMessage } = require("../../../utils/sendReaction");
+
 const handler = async (sock, msg, from, args, msgInfoObj) => {
     const { sendMessageWTyping, evv } = msgInfoObj;
+    reactToMessage(from, sock, msg, "🔄");
     if (!args[0]) return sendMessageWTyping(from, { text: 'Provide name.' }, { quoted: msg });
     try {
         await sock.groupUpdateSubject(from, evv);
+        await sendMessageWTyping(from, { text: `✅ Group name changed to ${args.join(" ")}` }, { quoted: msg });
+        reactToMessage(from, sock, msg, "✅");
     } catch (err) {
-        sendMessageWTyping(from, { text: err.toString() }, { quoted: msg });
+        reactToMessage(from, sock, msg, "❌");
+        sendMessageWTyping(from, { text: `❌ Error changing group name: ${err}` }, { quoted: msg });
     }
 }
 
